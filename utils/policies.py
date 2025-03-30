5862c4fb790ecf38d7f5e3850ca3fb78ffc8f23b
Vanh41 <vietanhm6a@gmail.com>
2025-03-30 13:04:23 +0700

    fix policies network

--- utils/policies.py
+++ utils/policies.py
@@ -7,17 +7,15 @@ import torch.nn as nn
 
 class ActorCritic(nn.Module):
     """
-    :param observation_space: The observation space of the environment
     :param features_dim: Number of features extracted.
         This corresponds to the number of unit for the last layer.
     """
     
-    def __init__(self,
-                 observation_space: gym.spaces,
+    def __init__(self, env,
                  features_dim: int = 512,
                  ):
         super().__init__()
-        n_input_channels = observation_space.shape[0]
+        n_input_channels = env.observation_space.shape[0]
         self.actor = nn.Sequential(
             nn.Conv2d(n_input_channels, 64, kernel_size=8, stride=4, padding=0),
             nn.ReLU(),
@@ -38,7 +36,7 @@ class ActorCritic(nn.Module):
         )
         # Compute shape by doing one forward pass
         with torch.no_grad():
-            n_flatten = self.actor(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
+            n_flatten = self.actor(torch.as_tensor(env.observation_space.sample()[None]).float()).shape[1]
             
         self.linearActor = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.Tanh())
         self.linearCritic = nn.Sequential(nn.Linear(n_flatten, features_dim), 1)
